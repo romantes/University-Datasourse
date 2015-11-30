1c955676f7566e5d798c4020bea4140ce79d9132
Roman Teschin <aquasky.dream@gmail.com>
2015-11-30 17:09:38 +0200

    OOP/University was deleted

--- OOP/Professor.java
+++
@@ -1,9 +0,0 @@
-package com.mentat.OOP.task6;
-
-public class Professor extends Person {
-
-	public Professor(long personId, String firstName, String secondName) {
-		super(personId, firstName, secondName);
-	}
-	
-}
