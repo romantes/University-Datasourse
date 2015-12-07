4165e1649c040b54c820ca6cfa2fb68aa020f965
Roman Teschin <aquasky.dream@gmail.com>
2015-12-07 10:20:41 +0200

    dev directiory cleaned

--- Professor.java
+++
@@ -1,9 +0,0 @@
-package com.mentat.University;
-
-public class Professor extends Person {
-
-	public Professor(long personId, String firstName, String secondName) {
-		super(personId, firstName, secondName);
-	}
-	
-}
